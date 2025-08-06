885ca3e86ee112a516e61da71f735cec5072a2c6
Pugazh Coder <pugazhstanly@gmail.com>
2025-08-06 22:30:40 +0530

    Update Suggestion.jsx
    Modify Suggestion

--- src/Suggestion.jsx
+++ src/Suggestion.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState, useEffect } from 'react';
 
 function Suggestion() {
+
+  const [profile, setProfile] = useState(null);
+  const [suggestion, setSuggestion] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pugazh529.github.io/HostData/profile.json')
+      .then((data) => data.json())
+      .then((data) => { setProfile(data) })
+      .catch((e) => console.log(e));
+  }, []);
+
+
+
+
   return (
-    <div>Suggestion</div>
+    <div>
+      {profile ?
+        <div className='d-flex profilepicusername' key={profile.id}>
+          <img className=' dp rounded-circle' src={profile.profilePicture} alt="Profileimg" />
+          <h5>{profile.username}</h5>
+        </div>
+        : <p>Loding</p>}
+    </div>
   )
 }
 
